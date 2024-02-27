fccb8fbc77509a6359f48192a875c9fb30e3d1fd
LeverUp-Dev <123243889+LeverUp-Dev@users.noreply.github.com>
2024-02-27 20:39:15 +0900

    Initial commit

---
+++ src/main/java/university_team/project2024_01/Project202401Application.java
@@ -0,0 +1,13 @@
+package university_team.project2024_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project202401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project202401Application.class, args);
+	}
+
+}
---
+++ src/test/java/university_team/project2024_01/Project202401ApplicationTests.java
@@ -0,0 +1,13 @@
+package university_team.project2024_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project202401ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
